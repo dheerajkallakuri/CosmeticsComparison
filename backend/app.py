2c08790027383dce77c2141e7210a409ac69af5d
kd_rowdy_boy <dheerajkallakuri@users.noreply.github.com>
2024-10-08 19:47:43 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,16 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from flask_cors import CORS
 from bson import ObjectId
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
 
 # MongoDB connection (update with your connection string)
-mongodb_uri = os.environ.get('MONGODB_URI')
+mongodb_uri = os.getenv("MONGODB_URI")
 client = MongoClient(mongodb_uri)
 db = client['cosmetics']  # Use test database
 collection = db['cosmetics']  # Use cosmetics collection
